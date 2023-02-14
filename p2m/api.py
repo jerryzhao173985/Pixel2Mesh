1f0ac6275ea7e417f3f4a7d22cda561de8c6eb44
jerry <jerryzhao173985@gmail.com>
2023-02-14 07:47:47 +0000

    python3

--- p2m/api.py
+++ p2m/api.py
@@ -22,161 +22,161 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 class Model(object):
-    def __init__(self, **kwargs):
-        allowed_kwargs = {'name', 'logging'}
-        for kwarg in kwargs.keys():
-            assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
-        name = kwargs.get('name')
-        if not name:
-            name = self.__class__.__name__.lower()
-        self.name = name
+	def __init__(self, **kwargs):
+		allowed_kwargs = {'name', 'logging'}
+		for kwarg in kwargs.keys():
+			assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
+		name = kwargs.get('name')
+		if not name:
+			name = self.__class__.__name__.lower()
+		self.name = name
 
-        logging = kwargs.get('logging', False)
-        self.logging = logging
+		logging = kwargs.get('logging', False)
+		self.logging = logging
 
-        self.vars = {}
-        self.placeholders = {}
+		self.vars = {}
+		self.placeholders = {}
 
-        self.layers = []
-        self.activations = []
+		self.layers = []
+		self.activations = []
 
-        self.inputs = None
-        self.output1 = None
-        self.output2 = None
-        self.output3 = None
-        self.output1_2 = None
-        self.output2_2 = None
+		self.inputs = None
+		self.output1 = None
+		self.output2 = None
+		self.output3 = None
+		self.output1_2 = None
+		self.output2_2 = None
 
-        self.loss = 0
-        self.optimizer = None
-        self.opt_op = None
+		self.loss = 0
+		self.optimizer = None
+		self.opt_op = None
 
-    def _build(self):
-        raise NotImplementedError
+	def _build(self):
+		raise NotImplementedError
 
-    def build(self):
-        """ Wrapper for _build() """
-        #with tf.device('/gpu:0'):
-        with tf.variable_scope(self.name):
-            self._build()
+	def build(self):
+		""" Wrapper for _build() """
+		#with tf.device('/gpu:0'):
+		with tf.variable_scope(self.name):
+			self._build()
 
-        # Build sequential resnet model
-        eltwise = [3,5,7,9,11,13, 19,21,23,25,27,29, 35,37,39,41,43,45]
-        concat = [15, 31]
-        self.activations.append(self.inputs)
-        for idx,layer in enumerate(self.layers):
-            hidden = layer(self.activations[-1])
-            if idx in eltwise:
-                hidden = tf.add(hidden, self.activations[-2]) * 0.5
-            if idx in concat:
-                hidden = tf.concat([hidden, self.activations[-2]], 1)
-            self.activations.append(hidden)
+		# Build sequential resnet model
+		eltwise = [3,5,7,9,11,13, 19,21,23,25,27,29, 35,37,39,41,43,45]
+		concat = [15, 31]
+		self.activations.append(self.inputs)
+		for idx,layer in enumerate(self.layers):
+			hidden = layer(self.activations[-1])
+			if idx in eltwise:
+				hidden = tf.add(hidden, self.activations[-2]) * 0.5
+			if idx in concat:
+				hidden = tf.concat([hidden, self.activations[-2]], 1)
+			self.activations.append(hidden)
 
-        self.output1 = self.activations[15]
-        unpool_layer = GraphPooling(placeholders=self.placeholders, pool_id=1)
-        self.output1_2 = unpool_layer(self.output1)
+		self.output1 = self.activations[15]
+		unpool_layer = GraphPooling(placeholders=self.placeholders, pool_id=1)
+		self.output1_2 = unpool_layer(self.output1)
 
-        self.output2 = self.activations[31]
-        unpool_layer = GraphPooling(placeholders=self.placeholders, pool_id=2)
-        self.output2_2 = unpool_layer(self.output2)
+		self.output2 = self.activations[31]
+		unpool_layer = GraphPooling(placeholders=self.placeholders, pool_id=2)
+		self.output2_2 = unpool_layer(self.output2)
 
-        self.output3 = self.activations[-1]
+		self.output3 = self.activations[-1]
 
-        # Store model variables for easy access
-        variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
-        self.vars = {var.name: var for var in variables}
+		# Store model variables for easy access
+		variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
+		self.vars = {var.name: var for var in variables}
 
-    def predict(self):
-        pass
+	def predict(self):
+		pass
 
-    def _loss(self):
-        raise NotImplementedError
+	def _loss(self):
+		raise NotImplementedError
 
-    def save(self, sess=None):
-        if not sess:
-            raise AttributeError("TensorFlow session not provided.")
-        saver = tf.train.Saver(self.vars)
-        save_path = saver.save(sess, "Data/checkpoint/%s.ckpt" % self.name)
-        print("Model saved in file: %s" % save_path)
+	def save(self, sess=None):
+		if not sess:
+			raise AttributeError("TensorFlow session not provided.")
+		saver = tf.train.Saver(self.vars)
+		save_path = saver.save(sess, "Data/checkpoint/%s.ckpt" % self.name)
+		print("Model saved in file: %s" % save_path)
 
-    def load(self, sess=None):
-        if not sess:
-            raise AttributeError("TensorFlow session not provided.")
-        saver = tf.train.Saver(self.vars)
-        save_path = "Data/checkpoint/%s.ckpt" % self.name
-        #save_path = "checks/tmp/%s.ckpt" % self.name
-        saver.restore(sess, save_path)
-        print("Model restored from file: %s" % save_path)
+	def load(self, sess=None):
+		if not sess:
+			raise AttributeError("TensorFlow session not provided.")
+		saver = tf.train.Saver(self.vars)
+		save_path = "Data/checkpoint/%s.ckpt" % self.name
+		#save_path = "checks/tmp/%s.ckpt" % self.name
+		saver.restore(sess, save_path)
+		print("Model restored from file: %s" % save_path)
 
 class GCN(Model):
-    def __init__(self, placeholders, **kwargs):
-        super(GCN, self).__init__(**kwargs)
+	def __init__(self, placeholders, **kwargs):
+		super(GCN, self).__init__(**kwargs)
 
-        self.inputs = placeholders['features']
-        self.placeholders = placeholders
+		self.inputs = placeholders['features']
+		self.placeholders = placeholders
 
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
 
-        self.build()
+		self.build()
 
-    def _build(self):
-        self.build_cnn18() #update image feature
+	def _build(self):
+		self.build_cnn18() #update image feature
 		# first project block
-        self.layers.append(GraphProjection(placeholders=self.placeholders))
-        self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim,
-                                            output_dim=FLAGS.hidden,
-                                            gcn_block_id=1,
-                                            placeholders=self.placeholders, logging=self.logging))
-        for _ in range(12):
-            self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                                output_dim=FLAGS.hidden,
-                                                gcn_block_id=1,
-                                                placeholders=self.placeholders, logging=self.logging))
-        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                            output_dim=FLAGS.coord_dim,
-                                            act=lambda x: x,
-                                            gcn_block_id=1,
-                                            placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphProjection(placeholders=self.placeholders))
+		self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim,
+											output_dim=FLAGS.hidden,
+											gcn_block_id=1,
+											placeholders=self.placeholders, logging=self.logging))
+		for _ in range(12):
+			self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+												output_dim=FLAGS.hidden,
+												gcn_block_id=1,
+												placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+											output_dim=FLAGS.coord_dim,
+											act=lambda x: x,
+											gcn_block_id=1,
+											placeholders=self.placeholders, logging=self.logging))
 		# second project block
-        self.layers.append(GraphProjection(placeholders=self.placeholders))
-        self.layers.append(GraphPooling(placeholders=self.placeholders, pool_id=1)) # unpooling
-        self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim+FLAGS.hidden,
-                                            output_dim=FLAGS.hidden,
-                                            gcn_block_id=2,
-                                            placeholders=self.placeholders, logging=self.logging))
-        for _ in range(12):
-            self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                                output_dim=FLAGS.hidden,
-                                                gcn_block_id=2,
-                                                placeholders=self.placeholders, logging=self.logging))
-        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                            output_dim=FLAGS.coord_dim,
-                                            act=lambda x: x,
-                                            gcn_block_id=2,
-                                            placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphProjection(placeholders=self.placeholders))
+		self.layers.append(GraphPooling(placeholders=self.placeholders, pool_id=1)) # unpooling
+		self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim+FLAGS.hidden,
+											output_dim=FLAGS.hidden,
+											gcn_block_id=2,
+											placeholders=self.placeholders, logging=self.logging))
+		for _ in range(12):
+			self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+												output_dim=FLAGS.hidden,
+												gcn_block_id=2,
+												placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+											output_dim=FLAGS.coord_dim,
+											act=lambda x: x,
+											gcn_block_id=2,
+											placeholders=self.placeholders, logging=self.logging))
 		# third project block
-        self.layers.append(GraphProjection(placeholders=self.placeholders))
-        self.layers.append(GraphPooling(placeholders=self.placeholders, pool_id=2)) # unpooling
-        self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim+FLAGS.hidden,
-                                            output_dim=FLAGS.hidden,
-                                            gcn_block_id=3,
-                                            placeholders=self.placeholders, logging=self.logging))
-        for _ in range(12):
-            self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                                output_dim=FLAGS.hidden,
-                                                gcn_block_id=3,
-                                                placeholders=self.placeholders, logging=self.logging))
-        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
-                                            output_dim=int(FLAGS.hidden/2),
-                                            gcn_block_id=3,
-                                            placeholders=self.placeholders, logging=self.logging))
-        self.layers.append(GraphConvolution(input_dim=int(FLAGS.hidden/2),
-                                            output_dim=FLAGS.coord_dim,
-                                            act=lambda x: x,
-                                            gcn_block_id=3,
-                                            placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphProjection(placeholders=self.placeholders))
+		self.layers.append(GraphPooling(placeholders=self.placeholders, pool_id=2)) # unpooling
+		self.layers.append(GraphConvolution(input_dim=FLAGS.feat_dim+FLAGS.hidden,
+											output_dim=FLAGS.hidden,
+											gcn_block_id=3,
+											placeholders=self.placeholders, logging=self.logging))
+		for _ in range(12):
+			self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+												output_dim=FLAGS.hidden,
+												gcn_block_id=3,
+												placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphConvolution(input_dim=FLAGS.hidden,
+											output_dim=int(FLAGS.hidden/2),
+											gcn_block_id=3,
+											placeholders=self.placeholders, logging=self.logging))
+		self.layers.append(GraphConvolution(input_dim=int(FLAGS.hidden/2),
+											output_dim=FLAGS.coord_dim,
+											act=lambda x: x,
+											gcn_block_id=3,
+											placeholders=self.placeholders, logging=self.logging))
 
-    def build_cnn18(self):
+	def build_cnn18(self):
 		x=self.placeholders['img_inp']
 		x=tf.expand_dims(x, 0)
 #224 224
